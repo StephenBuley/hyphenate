edc35a043a9a14965b5a310af3fcd53602bba898
Stephen Buley <stephen.a.buley@gmail.com>
2023-04-18 16:35:52 +0200

    add output and change colors
    Hyphenate now outputs a success message and colors the files so that you
    can more easily tell what happened.

--- hyphenate.sh
+++ hyphenate.sh
@@ -11,7 +11,10 @@ echo $1 > temp.txt
 newFileName=$(sed 's/\ /-/g' temp.txt)
 
 mv "$1" $newFileName
-
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+echo -e "changed '${RED}$1${NC}' to '${GREEN}$newFileName${NC}'"
 
 # cleanup
 rm temp.txt
